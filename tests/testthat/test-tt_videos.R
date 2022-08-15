20abd7b31a11a8e5d9785ab7b3036f54e26d51a5
JBGruber <gruber@europa-uni.de>
2022-08-15 07:50:08 +0200

    fixed test

--- tests/testthat/test-tt_videos.R
+++ tests/testthat/test-tt_videos.R
@@ -1,7 +1,6 @@
 skip_if(Sys.getenv("TT_COOKIES") == "")
 df <- tt_videos(video_urls = c("https://www.tiktok.com/@tiktok/video/7125860750463094058?is_copy_url=1&is_from_webapp=v1",
-                               "https://www.tiktok.com/@tiktok/video/7125860750463094058?is_copy_url=1&is_from_webapp=v1",
-                               "https://www.tiktok.com/"),
+                               "https://www.tiktok.com/@tiktok/video/7125860750463094058?is_copy_url=1&is_from_webapp=v1"),
                 save_video = TRUE,
                 dir = tempdir(),
                 cookiefile = Sys.getenv("TT_COOKIES"))
@@ -11,4 +10,7 @@ test_that("get meta and download", {
   expect_equal(nrow(df), 2L)
   expect_equal(ncol(df), 19L)
   expect_equal(file.exists(df[["video_fn"]][1]), TRUE)
+  expect_warning(tt_videos("https://www.tiktok.com/",
+                           cookiefile = Sys.getenv("TT_COOKIES")),
+                 "https://www.tiktok.com/ can't be reached.")
 })
