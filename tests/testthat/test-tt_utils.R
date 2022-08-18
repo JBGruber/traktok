b13004330a60c628a6d5bff0bafc859ce7a451ee
JBGruber <gruber@europa-uni.de>
2022-08-18 10:26:42 +0200

    Fixed failing test

--- tests/testthat/test-tt_utils.R
+++ tests/testthat/test-tt_utils.R
@@ -1,5 +1,5 @@
 test_that("wait", {
-  expect_message(traktok:::wait(1:2), "...waiting \\d+\\.\\d seconds")
+  expect_message(traktok:::wait(1:2), "...waiting (\\d+\\.\\d|\\d+) seconds")
   expect_error(traktok:::tt_read_cookies("test"),
                "cookiefile test does not exist")
   expect_equal(
